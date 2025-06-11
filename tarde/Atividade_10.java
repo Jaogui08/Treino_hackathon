bc826e75bce06a98da910164ca8c4cebad844f34
Jaogui08 <salesfabriciojg@gmail.com>
2025-06-11 15:10:30 -0300

    treino hackathon

--- tarde/Atividade_10.java
+++ tarde/Atividade_10.java
@@ -1,10 +1,35 @@
 package hackathon_treino.tarde;
 
+import java.util.Scanner;
+
 public class Atividade_10 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("Escolha um número entre 1 a 4: ");
+		int numeroAtaque = scanner.nextInt();
+		
+		switch (numeroAtaque) {
+		
+		case 1:
+			System.out.println("\nInvestida");
+			break;
+		case 2:
+			System.out.println("\nRajada de Fumaça");
+			break;
+		case 3:
+			System.out.println("\nChoque do Trovão");
+			break;
+		case 4:
+			System.out.println("\nHidro Bomba");
+			break;
+		default:
+			System.out.println("\nAtaque desconheido");
+			break;
+		}
+		
+		scanner.close();
 	}
 
 }
--- tarde/Atividade_9.java
+++ tarde/Atividade_9.java
@@ -1,10 +1,32 @@
 package hackathon_treino.tarde;
 
+import java.util.Scanner;
+
 public class Atividade_9 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("Informe o nome de um pokémon: ");
+		String pokeNome = scanner.next().toLowerCase();
+		
+		switch (pokeNome) {
+		
+		case "bulbassaur":
+			System.out.println("\nA região de origem desse pokémon é Kanto");
+			break;
+		case "chimchar":
+			System.out.println("\nA região de origem desse pokémon é Sinnoh");
+			break;
+		case "torchic":
+			System.out.println("\nA região de origem desse pokémon é Hoenn");
+			break;
+		default:
+			System.out.println("\nA região de origem desse pokémon é desconhecida");
+			break;
+		}
+		
+		scanner.close();
 	}
 
 }
